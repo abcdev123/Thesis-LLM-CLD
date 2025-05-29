2e424237a3fbe4448bcda44bd6a0d4c2eb4a3a12
abcdev123 <gijsschouten@yahoo.com>
2025-05-29 10:33:21 +0200

    test inference on non fine-tuned base model

--- src/test_inference.py
+++ src/test_inference.py
@@ -3,7 +3,8 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # ─── CONFIG ─────────────────────────────────────────────────────────────────────
-MODEL_DIR      = "src/Mistral_LLM_7B_v0.1_Base_lora_finetuned/merged_fp16_7Bv0.1"
+# MODEL_DIR      = "src/Mistral_LLM_7B_v0.1_Base_lora_finetuned/merged_fp16_7Bv0.1"
+MODEL_DIR      = "mistralai/Mistral-7B-v0.1"
 DEVICE         = "cuda" if torch.cuda.is_available() else "cpu"
 DTYPE          = torch.float16 if DEVICE == "cuda" else torch.float32
 SEQ_LEN        = 1024
