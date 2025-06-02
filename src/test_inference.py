937e9038b32371b9d6617d4c0ba5b30349b0b768
abcdev123 <gijsschouten@yahoo.com>
2025-06-02 08:16:13 +0200

    test inference 14B model LoRA

--- src/test_inference.py
+++ src/test_inference.py
@@ -6,8 +6,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 # MODEL_DIR      = "mistralai/Mistral-7B-v0.1"            # base Mistral checkpoint
 # MODEL_DIR      = "src/Mistral_LLM_7B_Instruct-v0.2_lora_finetuned/merged_fp16"  # Path to your fine-tuned model
 # MODEL_DIR      = "src/Mistral_LLM_7B_Instruct-v0.2_lora_finetuned_w_wrapping/merged_fp16"
-# MODEL_DIR      = "src/Mistral_LLM_7B_Instruct-v0.2_Qlora_finetuned/merged_fp16"
-MODEL_DIR      = "src/Rombos-LLM-V2.5-Qwen-32b_lora_finetuned_w_wrapping/merged_fp16"
+MODEL_DIR      = "src/Qwen2.5-14B-Instruct_lora_finetuned_w_wrapping/merged_fp16"
+# MODEL_DIR      = "src/Rombos-LLM-V2.5-Qwen-32b_lora_finetuned_w_wrapping/merged_fp16"
 DEVICE         = "cuda" if torch.cuda.is_available() else "cpu"
 DTYPE          = torch.float16 if DEVICE == "cuda" else torch.float32
 SEQ_LEN        = 2048
